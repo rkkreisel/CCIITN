eb134af52ac62d1b360c3a326e029767ed270bc5
Brandon Kreisel <kreiselb@gmail.com>
2018-02-16 06:23:11 -0500

    Import Utils from Second30

--- config.py
+++ config.py
@@ -1,7 +1,22 @@
-VERSION = "0.0.1"
-LOGFILE = "trader.log"
+""" CCI ITN Configuration Values """
+from datetime import datetime
+########## CONFIGURATION ##########
+
+VERSION = "0.1"
+LOGFILE = "ccitn_{}.log".format(datetime.now().strftime('%Y%m%d_%H%M'))
+LEDGERFILE = "cciitn_trades.csv"
 
 HOST = "localhost"
-PORT = 44444
+PORT = 55555
+CLIENTID = 2
 
-SYMBOL= "ES"
+NUM_CONTRACTS = 1
+
+SYMBOL = "ES"
+EXCHANGE = "GLOBEX"
+
+#Normal Timezone is CST.
+NORMAL_TRADING_HOURS = "1700-1515,1530-1600"
+
+# Type of Market Data To Stream. (LIVE, DELAYED, FROZEN_DELAYED)
+DATATYPE = "LIVE"
