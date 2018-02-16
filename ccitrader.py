e8a3bc1e0e6792704b61f8cddbcdbac768400263
Brandon Kreisel <kreiselb@gmail.com>
2018-02-16 06:29:17 -0500

    Main Client

--- ccitrader.py
+++ ccitrader.py
@@ -1,92 +1,73 @@
-import argparse
-import os
+""" CCI ITN Algo Connection Manager and Thread Handler"""
+
+########## STDLIB IMPORTS ##########
 import sys
 import signal
-import pdb
 
-import ibapi
+########## CUSTOM IMPORTS ##########
+import config
+
 from ibapi.client import EClient
 
+from logger import setupLogger, getConsole as console
 from wrapper import AppWrapper
-import config
-import multiprocessing
-
-import logging
+from request_manager import RequestManager
+from ledger import Ledger
 
 ########## CLASSES ##########
 class AppClient(EClient):
+    """ IBAPI App Client """
     def __init__(self, wrapper):
         EClient.__init__(self, wrapper)
-                    
-class CCITrader(AppWrapper, AppClient):
+
+class CCITrader(AppWrapper, AppClient, RequestManager):
+    """ Client, Wrapper, and Logic Bundler """
     def __init__(self):
         AppWrapper.__init__(self)
         AppClient.__init__(self, wrapper=self)
-        signal.signal(signal.SIGINT, self.interruptHandler) # CTRL + C
-        #signal.signal(signal.SIGTSTP, self.debugHandler) # CTRL + Z
-        #signal.signal(signal.SIGINFO, self.statusHandler) #CTRL + T
+        RequestManager.__init__(self)
+        self.ledger = Ledger()
+        try:
+            signal.signal(signal.SIGINT, self.interruptHandler)
+            signal.signal(signal.SIGTSTP, self.statusHandler)
+        except AttributeError:
+            console().warning("Warning. Unable to Bind a Signal Handler.")
 
-    def interruptHandler(self, signum, frame):
-        console = logging.getLogger(name="console")
-        if len(self.logic.subscriptions) > 0:
-            console.info("Stopping Streaming Market Subscriptions...")
-            for id, data in self.logic.subscriptions.items():
-                console.info("Stopping Subscription: {}".format(data.summary.localSymbol))
-                self.cancelMktData(id)
-        console.info("Disconnecting From API...")
+
+    def interruptHandler(self, *_):
+        """ Gracefully quit on CTRL+C """
+        console().info("Disconnecting From API...")
+        self.stopAllSubscriptions()
         self.disconnect()
+        self.ledger.close()
         sys.exit(0)
 
-    def debugHandler(self, signum, frame):
-        console = logging.getLogger(name="console")
-        console.info("Starting Debugging Console...")
-        import pdb;pdb.set_trace()
-        
-    def statusHandler(self, signum, frame):
-        self.logic.printPriceData()  
-                    
-########## FUNCTIONS ##########
-def setupLogger():
-    
-    if os.path.exists(config.LOGFILE):
-        os.remove(config.LOGFILE)
-    
-    logformat = "%(asctime)s -  %(funcName)s::%(lineno)d [%(threadName)s]: %(message)s"
-    
-    filelogger = logging.getLogger()
-    logging.basicConfig(
-        filename = config.LOGFILE,
-        level = logging.INFO, 
-        format = logformat)
-        
-    console = logging.getLogger(name="console")
-    console_handler = logging.StreamHandler(stream=sys.stdout)
-    console_handler.setFormatter(logging.Formatter(logformat))
-    console.addHandler(console_handler)
-    
-    return console
-    
+    def statusHandler(self, *_):
+        """ Show Price and Other Data on Ctrl + Z """
+        self.printStatus()
+
 ########## MAIN ##########
 def main():
-    console = setupLogger()
-    console.info("Started CCITrader v{}".format(config.VERSION))
-    
+    """ Setup Logging and Intialize Algo Trader """
+
+    setupLogger()
+    console().info("Started CCI ITN Trader v{}".format(config.VERSION))
+
     app = CCITrader()
-    
+
     try:
-        console.info("Connecting to TWS API  on HOST {} and PORT {}".format(config.HOST,config.PORT))
-        console.info("Using Interactive Brokers API Version: {}".format(ibapi.get_version_string()))
-        app.connect(config.HOST, config.PORT, clientId=0)
+        console().info("Connecting to TWS API at {}:{}. Client ID: {}"
+                       .format(config.HOST, config.PORT, config.CLIENTID))
+
+        app.connect(config.HOST, config.PORT, clientId=config.CLIENTID)
+
         if app.isConnected():
-            console.info("Connection Successful.  Server Version: {}".format(app.serverVersion()))
-            try:
-                app.run()
-            except AttributeError:
-                pass
+            console().info("Connection Successful.  Server Version: {}".format(app.serverVersion()))
+            app.run()
         else:
-            console.info("Connection Failed")
+            console().info("Connection Failed")
     except:
         raise
-        
+
 if __name__ == "__main__":
     main()
